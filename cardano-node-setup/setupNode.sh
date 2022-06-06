325b74ba237a78ae6d0d2c1e9277c9e885da3dee
Ülla <90785712+ullamonster@users.noreply.github.com>
2022-06-06 01:26:09 +0000

    Update setupNode.sh

--- cardano-node-setup/setupNode.sh
+++ cardano-node-setup/setupNode.sh
@@ -1,15 +1,15 @@
 ### Install Cabal
 cd $HOME
 ghcup upgrade
-ghcup install cabal 3.4.0.0
-ghcup set cabal 3.4.0.0
+ghcup install cabal 3.6.2.0
+ghcup set cabal 3.6.2.0
 
 ### Install GHC
 ghcup install ghc 8.10.7
 ghcup set ghc 8.10.7
 
 ### Update Cabal and verify versions
-### IMPORTANT: Cabal should be at version 3.4.0.0 and GHC should be at version 8.10.7
+### IMPORTANT: Cabal should be at version 3.6.2.0 and GHC should be at version 8.10.7
 cabal update
 cabal --version
 ghc --version
